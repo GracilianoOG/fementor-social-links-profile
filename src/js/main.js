3e2b7cd4361ef754567581046a5aae924be17096
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-30 17:19:25 -0300

    Add alt text to the image

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import '../css/style.css'
 
 document.querySelector('#app').innerHTML = `
   <div class="card">
-    <img class="card__avatar" src="../assets/images/avatar-jessica.jpeg" alt="" />
+    <img class="card__avatar" src="../assets/images/avatar-jessica.jpeg" alt="Portrait of a confident black woman with a stylish afro hairstyle, making direct eye contact with the camera." />
     <h1 class="card__title">Jessica Randall</h1>
     <h2 class="card__location">London, United Kingdom</h2>
     <p class="card__description">"Front-end developer and avid reader."</p>
