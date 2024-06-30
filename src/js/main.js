48a5ad493191761a39da57059232c033ae975edb
Gabriel <gabriel.gracilianomb@gmail.com>
2024-06-30 17:44:22 -0300

    Change card tag to article

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import '../css/style.css'
 
 document.querySelector('#app').innerHTML = `
-  <div class="card">
+  <article class="card">
     <img class="card__avatar" src="../assets/images/avatar-jessica.jpeg" alt="Portrait of a confident black woman with a stylish afro hairstyle, making direct eye contact with the camera." />
     <h1 class="card__title">Jessica Randall</h1>
     <h2 class="card__location">London, United Kingdom</h2>
@@ -12,5 +12,5 @@ document.querySelector('#app').innerHTML = `
     <a class="card__social" href="#">LinkedIn</a>
     <a class="card__social" href="#">Twitter</a>
     <a class="card__social" href="#">Instagram</a>
-  </div>
+  </article>
 `
