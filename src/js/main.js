5cef6566a8426d6a83ae7ab2dd4430863bd457d7
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-01 12:22:25 -0300

    Add main tag as wrapper

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,18 @@
 import '../css/style.css'
 
 document.querySelector('#app').innerHTML = `
-  <article class="card">
-    <img class="card__avatar" src="../assets/images/avatar-jessica.jpeg" alt="Portrait of a confident black woman with a stylish afro hairstyle, making direct eye contact with the camera." />
-    <h1 class="card__title">Jessica Randall</h1>
-    <h2 class="card__location">London, United Kingdom</h2>
-    <p class="card__description">"Front-end developer and avid reader."</p>
+  <main class="wrapper">
+    <article class="card">
+      <img class="card__avatar" src="../assets/images/avatar-jessica.jpeg" alt="Portrait of a confident black woman with a stylish afro hairstyle, making direct eye contact with the camera." />
+      <h1 class="card__title">Jessica Randall</h1>
+      <h2 class="card__location">London, United Kingdom</h2>
+      <p class="card__description">"Front-end developer and avid reader."</p>
 
-    <a class="card__social" href="#">GitHub</a>
-    <a class="card__social" href="#">Frontend Mentor</a>
-    <a class="card__social" href="#">LinkedIn</a>
-    <a class="card__social" href="#">Twitter</a>
-    <a class="card__social" href="#">Instagram</a>
-  </article>
+      <a class="card__social" href="#">GitHub</a>
+      <a class="card__social" href="#">Frontend Mentor</a>
+      <a class="card__social" href="#">LinkedIn</a>
+      <a class="card__social" href="#">Twitter</a>
+      <a class="card__social" href="#">Instagram</a>
+    </article>
+  </main>
 `
